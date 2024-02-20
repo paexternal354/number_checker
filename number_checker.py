e8efcaed7a943172974940e35967b2e25166c4a4
paexternal354 <160279365+paexternal354@users.noreply.github.com>
2024-02-21 03:37:32 +1100

    Add files via upload

---
+++ number_checker.py
@@ -0,0 +1,8 @@
+# Ask the user to enter a number
+number = int(input("Enter a number: "))
+
+# Check if the number is even or not
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    pass
